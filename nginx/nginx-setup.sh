1629dc51933bd2b77cd5dced8b04af887d1c46ec
Evan Jackson <evan.jackson@torqit.ca>
2023-07-27 13:24:06 -0300

    Workaround so script can continue

--- nginx/nginx-setup.sh
+++ nginx/nginx-setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo Setting up nginx reverse proxy... 
+# Log into the group without forcing the user to logout/login
+newgrp docker
 docker network create nginx_proxy
 docker compose up -d
