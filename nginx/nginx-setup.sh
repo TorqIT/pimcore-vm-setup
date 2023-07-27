8d81cac3cbecc03f9fad539bda93ba34aea60ad1
Evan Jackson <evan.jackson@torqit.ca>
2023-07-27 13:26:20 -0300

    Documentation

--- nginx/nginx-setup.sh
+++ nginx/nginx-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo Setting up nginx reverse proxy... 
+echo Setting up NGINX reverse proxy... 
 # Log into the group without forcing the user to logout/login
 newgrp docker
 docker network create nginx_proxy
 docker compose up -d
+echo Finished setting up NGINX
