cb598f095b3d8b0df5aee15a65b38bc7399a1fca
Evan Jackson <evan.jackson@torqit.ca>
2023-07-27 15:40:43 -0300

    Removing setup-all for now and removing newgrp command as it crashes things

--- nginx/nginx-setup.sh
+++ nginx/nginx-setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 echo Setting up NGINX reverse proxy... 
-# Log into the group without forcing the user to logout/login
-newgrp docker
 docker network create nginx_network
 docker compose up -d
 echo Finished setting up NGINX
