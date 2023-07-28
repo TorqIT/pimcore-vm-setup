c0ee87dd9b9b49fdc06aadf9483a309331bf4ff4
Evan Jackson <evan.jackson@torqit.ca>
2023-07-28 11:57:40 -0300

    Sudo for docker stuff

--- nginx/nginx-setup.sh
+++ nginx/nginx-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo Setting up NGINX reverse proxy... 
-docker network create nginx_network
-docker compose up -d
+sudo docker network create nginx_network
+sudo docker compose up -d
 echo Finished setting up NGINX
