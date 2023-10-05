ed744a618f0706d2a6ac79591c91e7785a6ce574
Evan Jackson <evan.jackson@torqit.ca>
2023-10-05 10:47:42 -0300

    Several changes to make runner script work

--- github-actions-runner/runner-setup.sh
+++ github-actions-runner/runner-setup.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+	echo "Not running as root"
+	exit
+fi
+
 echo Setting this VM up as a self-hosted GitHub Runner...
 
 while [[ "$#" -gt 0 ]]
   do
     case $1 in
       -r|--repo) Repo="$2"; shift;;
-      -version|--version) Version="$2"; shift;;
-      -token|--token) Token="$2"; shift;;
+      -v|--version) Version="$2"; shift;;
+      -t|--token) Token="$2"; shift;;
+	  -u|--user) RunnerUser="$2"; shift;;
     esac
     shift
 done
 
-DIR=/home/$USER
+DIR=/home/$RunnerUser
 cd $DIR
 
 echo Downloading GitHub Runner installer...
@@ -18,8 +28,9 @@ curl -o actions-runner-linux-x64-$Version.tar.gz -L https://github.com/actions/r
 
 echo Installing Runner as a service...
 tar xzf ./actions-runner-linux-x64-$Version.tar.gz
-./config.sh --url $Repo --token $Token
-./svc.sh install $USER
-./svc.sh start $USER
+chown -R $RunnerUser:$RunnerUser /home/$RunnerUser
+runuser -u $RunnerUser -- ./config.sh --url $Repo --token $Token
+./svc.sh install $RunnerUser
+./svc.sh start $RunnerUser
 
 echo Finished setting up GitHub Runner
