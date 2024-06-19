62e51fb4fddd4472fdc8c3f1c4ce662351dff266
Evan Jackson <evan.jackson@torqit.ca>
2024-06-19 15:33:25 -0300

    Update pimcore-setup.sh

--- pimcore/pimcore-setup.sh
+++ pimcore/pimcore-setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo Adding user pimcore with UID $1 and GID $2...
-sudo useradd -u $1 -g $2 pimcore
-
 echo Creating external Docker networks for blue/green deployments...
 docker network create database-network
 docker network create redis-network
